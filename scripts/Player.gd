1c6817f65b3adb65ee88e0b806df789c37c10abc
Siavash <siavash2samiei@gmail.com>
2021-06-12 18:01:59 -0400

    Added base code for player movement

---
+++ scripts/Player.gd
@@ -0,0 +1,42 @@
+extends "res://scripts/Character.gd"
+
+
+const MAX_JUMP_TIME =  0.1
+
+# Check whether or not a character is on the ground.
+var grounded = false 
+var can_jump = false
+var jump_time = 0
+
+func _ready():
+	# Player specific movement values.
+	var acceleration = 1000
+	var max_move_speed = 200
+	var max_jump_speed = 400
+	
+func apply_force(state: Physics2DDirectBodyState) -> void:
+	if(Input.is_action_pressed("ui_left")):
+		directional_force += Vector2.LEFT
+		
+	if(Input.is_action_pressed("ui_right")):
+		directional_force += Vector2.RIGHT
+	
+	if(Input.is_action_pressed("ui_up") && jump_time < MAX_JUMP_TIME && can_jump):
+		directional_force += Vector2.UP
+		jump_time += state.get_step()
+		
+	elif(Input.is_action_just_released("ui_up")):
+		can_jump = false
+		pass
+	if(grounded):
+		# Reset jumping.
+		can_jump = true
+		jump_time = 0
+
+
+func _on_GroundCheck_body_entered(body: Node) -> void:
+	pass # Replace with function body.
+
+
+func _on_GroundCheck_body_exited(body: Node) -> void:
+	pass # Replace with function body.
