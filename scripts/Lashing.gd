85e283a94ae51942b3b38c73c9e49e02dffcf0c6
Kevin Covelli <kevin.covelli@gmail.com>
2021-06-11 23:34:47 -0400

    basic lashing script

---
+++ scripts/Lashing.gd
@@ -0,0 +1,26 @@
+extends Node
+
+export(NodePath) var pb1_node_path;
+export(NodePath) var pb2_node_path;
+
+var pb1: RigidBody2D;
+var pb2: RigidBody2D;
+
+var strength: float = 500.0
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pb1 = get_node(pb1_node_path)
+	pb2 = get_node(pb2_node_path)
+	print(pb1, pb2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pb1.set_applied_force(pb1.position.direction_to(pb2.position) * strength)
+	pb2.set_applied_force(pb2.position.direction_to(pb1.position) * strength)
+	
