842a1051035016ae51a8c80f1a2b079426ced7d5
Kevin Covelli <kevin.covelli@gmail.com>
2021-06-11 23:42:42 -0400

    more boxx

--- scripts/Lashing.gd
+++ scripts/Lashing.gd
@@ -6,11 +6,7 @@ export(NodePath) var pb2_node_path;
 var pb1: RigidBody2D;
 var pb2: RigidBody2D;
 
-var strength: float = 500.0
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var strength: float = 10
 
 
 # Called when the node enters the scene tree for the first time.
@@ -21,6 +17,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pb1.set_applied_force(pb1.position.direction_to(pb2.position) * strength)
-	pb2.set_applied_force(pb2.position.direction_to(pb1.position) * strength)
+	pb1.apply_central_impulse(pb1.position.direction_to(pb2.position) * strength)
+	pb2.apply_central_impulse(pb2.position.direction_to(pb1.position) * strength)
 	
