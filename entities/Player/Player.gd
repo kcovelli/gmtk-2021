5ef57d37f8811ec45a742692066e2ca44ccab555
Kevin Covelli <kevin.covelli@gmail.com>
2021-06-17 21:17:17 -0400

    added damping to player while lashing

--- entities/Player/Player.gd
+++ entities/Player/Player.gd
@@ -8,10 +8,12 @@ const LASHED_UNITS_PER_FRAME = 25 # TODO: implement and test value
 const JUMP_FORCE = 300
 
 # Dampening values
-const AIRIAL_LINEAR_DAMP = 0.1
 const GROUND_LINEAR_DAMP = 0.1
-const AIRIAL_ANGULAR_DAMP = 5
 const GROUND_ANGULAR_DAMP = 5
+const AIRIAL_LINEAR_DAMP = 0.1
+const AIRIAL_ANGULAR_DAMP = 5
+const LASHING_LINEAR_DAMP = 1
+const LASHING_ANGULAR_DAMP = 8
 
 # Maximum speed that can be contributed by user input
 const MAX_USER_SPEED = 200
@@ -21,7 +23,7 @@ const MAX_USER_SPEED_SQ = MAX_USER_SPEED * MAX_USER_SPEED
 const MAX_GROUNDED_NORM_ANGLE = PI/8
 
 # reference to the player's rigidbody. no idea why we can't just access the fields directly but whatever
-onready var RB := $'.'
+onready var rb := $'.'
 
 # current 
 var curr_user_speed := 0
@@ -38,19 +40,25 @@ var all_norms = []
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	
-	# get most vertical norm and check if it is within the range to be considered grounded
+	# get norm from the "flattest" collision, and check if it is within the range to be considered grounded
 	contact_norm = get_most_vertical_norm(state)
 	grounded = abs(contact_norm.angle() + PI/2) < MAX_GROUNDED_NORM_ANGLE
 	
 	# apply dampening
-	if grounded:
-		RB.angular_damp = GROUND_ANGULAR_DAMP
-		RB.linear_damp = GROUND_LINEAR_DAMP
+	if is_lashing:
+		rb.angular_damp = LASHING_ANGULAR_DAMP
+		rb.linear_damp = LASHING_LINEAR_DAMP
+		print("player lashing")
+	elif grounded:
+		rb.angular_damp = GROUND_ANGULAR_DAMP
+		rb.linear_damp = GROUND_LINEAR_DAMP
 	else:
-		RB.angular_damp = AIRIAL_ANGULAR_DAMP
-		RB.linear_damp = AIRIAL_LINEAR_DAMP
+		rb.angular_damp = AIRIAL_ANGULAR_DAMP
+		rb.linear_damp = AIRIAL_LINEAR_DAMP
+	
 		
 	
+	# get user input, up/down not used for anything currently
 	var input := get_user_input()
 	var user_influence := Vector2(input[0], 0) * (GROUND_UNITS_PER_FRAME if grounded else AIR_UNITS_PER_FRAME)
 	var jump: bool = input[2]
@@ -72,7 +80,8 @@ func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	else:
 		calls_since_jump += 1
 	
-	$'/root/Main'.update()
+	if Globals.DRAW_DEBUG_NORMS:
+		$'/root/Main'.update()
 
 func get_user_input() -> Vector3:
 	var h = -1 if Input.is_action_pressed("ui_left") else 1 if Input.is_action_pressed("ui_right") else 0
@@ -80,6 +89,7 @@ func get_user_input() -> Vector3:
 	return Vector3(h, v, Input.is_action_pressed("jump"))
 
 
+# Loop over all reported collisions, and get the norm of the collision whose norm is closest to 90 deg from horizontal
 # TODO: this code looks gross, refactor
 func get_most_vertical_norm(state: Physics2DDirectBodyState) -> Vector2:
 	var n_norms = state.get_contact_count()
@@ -96,7 +106,8 @@ func get_most_vertical_norm(state: Physics2DDirectBodyState) -> Vector2:
 	for i in range(n_norms):
 		n = state.get_contact_local_normal(i)
 		all_norms.append(n)
-		a = abs(n.angle() + PI/2)
+		# n.angle() returns angle clockwise from x axis. don't remember why this is +PI/2 instead of -PI/2 then lmao
+		a = abs(n.angle() + PI/2) 
 		if a < min_angle:
 			min_norm = n
 			min_angle = a
